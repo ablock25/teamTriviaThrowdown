bbd84d69bfaaf22c19475f7b73f58f25918204d2
ablock25 <38792419+ablock25@users.noreply.github.com>
2021-10-26 19:19:30 -0400

    Initial commit and Animation

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,36 +1,114 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet } from 'react-native';
 import { AnimatedFade, AnimatedMove } from '@airship/rn-components';
 
 import { View } from '../components/common/View';
 import { Text } from '../components/common/Text';
 import { Screen } from '../components/common/Screen';
 import { Blimp } from '../assets/icons/Blimp';
-import { globalStyles } from '../styles/globalStyles';
+import { colors, globalStyles, SCREEN_HEIGHT } from '../styles/globalStyles';
+import { Loader } from '../components/common/Loader';
 
 export const HomeScreen = () => {
+  const [settled, setSettled] = useState(false);
+
   return (
     <Screen>
       <View style={styles.container}>
-        <AnimatedMove startY={-15}>
-          <AnimatedFade>
-            <Blimp containerStyle={styles.icon} />
+        <AnimatedMove
+          startY={-SCREEN_HEIGHT / 4}
+          delay={3000}
+          friction={0.5}
+          onEnd={() => {
+            setSettled(true);
+          }}
+        >
+          <AnimatedFade duration={3000}>
+            <Blimp containerStyle={styles.topIcon} size={150} />
+            {settled && (
+              <View style={styles.textWrapper}>
+                <AnimatedMove>
+                  <AnimatedFade>
+                    <Text style={styles.welcomeToText}>Welcome</Text>
+                  </AnimatedFade>
+                </AnimatedMove>
+                <AnimatedMove>
+                  <AnimatedFade>
+                    <Text style={styles.welcomeToText}>To</Text>
+                  </AnimatedFade>
+                </AnimatedMove>
+
+                <AnimatedMove>
+                  <AnimatedFade>
+                    <Text style={styles.triviaSmackdownText}>Trivia SMACKDOWN!</Text>
+                  </AnimatedFade>
+                </AnimatedMove>
+              </View>
+            )}
           </AnimatedFade>
         </AnimatedMove>
-        <Text>Welcome.</Text>
-        <Text>Let's get started.</Text>
+      </View>
+      <View style={styles.container}>
+        <AnimatedMove
+          friction={1}
+          tension={20}
+          delay={3000}
+          startY={SCREEN_HEIGHT / 4}
+          onEnd={() => {
+            setSettled(true);
+          }}
+        >
+          <AnimatedFade duration={3000}>
+            <Blimp containerStyle={styles.bottomIcon} size={150} color={colors.black} />
+            {/* <Button title={'start'} disabled={true} /> */}
+          </AnimatedFade>
+        </AnimatedMove>
       </View>
     </Screen>
   );
 };
 
 const styles = StyleSheet.create({
+  bottomIcon: {
+    marginVertical: globalStyles.standardPadding,
+    alignSelf: 'center',
+    justifyContent: 'flex-start',
+    flex: 2,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  icon: {
-    marginBottom: globalStyles.standardPadding * 2,
+  textWrapper: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginVertical: globalStyles.standardPadding,
+    alignItems: 'center',
+  },
+  topIcon: {
+    alignSelf: 'center',
+    justifyContent: 'flex-end',
+    flex: 2,
+  },
+  triviaSmackdownText: {
+    elevation: 100,
+    fontSize: 36,
+    fontWeight: 'bold',
+    textShadowRadius: 10,
+    textShadowColor: colors.orange,
+    shadowOpacity: 0.8,
+    flexWrap: 'wrap',
+    lineHeight: 38,
+  },
+  welcomeToText: {
+    elevation: 100,
+    fontSize: 28,
+    fontWeight: 'bold',
+    textShadowRadius: 6,
+    textShadowColor: colors.orange,
+    shadowOpacity: 0.8,
+    flexWrap: 'wrap',
+    lineHeight: 30,
   },
 });
